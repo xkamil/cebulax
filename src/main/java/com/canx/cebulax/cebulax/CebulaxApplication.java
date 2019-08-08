89dfaa72d5aad4e54aa2f49a30a4e0735b4cf152
Kamil <xkamilwrobel@gmail.com>
2019-08-08 20:47:09 +0200

    Initial commit

---
+++ src/main/java/com/canx/cebulax/cebulax/CebulaxApplication.java
@@ -0,0 +1,13 @@
+package com.canx.cebulax.cebulax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebulaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebulaxApplication.class, args);
+	}
+
+}
